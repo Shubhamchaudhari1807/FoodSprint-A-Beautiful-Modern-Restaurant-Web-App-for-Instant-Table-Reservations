40cad264740292a5f53d5882830e7e79d68b12e9
Shubham Chaudhari <131322489+Shubhamchaudhari1807@users.noreply.github.com>
2025-07-02 12:28:59 +0530

    Update Footer.jsx

--- MERN_STACK_RESTAURANT_RESERVATION-main/frontend/src/components/Footer.jsx
+++ MERN_STACK_RESTAURANT_RESERVATION-main/frontend/src/components/Footer.jsx
@@ -5,18 +5,22 @@ const Footer = () => {
     <footer>
       <div className="container">
         <div className="banner">
-          <div className="left">ZEESHU</div>
+          <div className="left">FoodSprint</div>
           <div className="right">
-            <p>Z6 Gulshan-e-Maymar, Karachi</p>
-            <p>Open: 05:00 PM - 12:00 AM</p>
+            <p>Shirpur, Maharashtra, 425405 India</p>
+            <p>Open: 10:00 AM - 10:00 PM</p>
           </div>
         </div>
         <div className="banner">
           <div className="left">
-            <p>Developed By CODEWITHZEESHU</p>
+            <p>Developed By Shubham 😊❤️😊</p>
+          </div>
+         
+        <div className="center">
+            <p> Copyright @2025 by  Shubham</p>
           </div>
           <div className="right">
-            <p>All Rights Reserved By CodeWithZeeshu.</p>
+            <p>All Rights Reserved By Shubham.</p>
           </div>
         </div>
       </div>
